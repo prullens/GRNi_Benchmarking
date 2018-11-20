5a0481db433a998ff15ed52f20913cc174c8c3d1
prullens <45201783+prullens@users.noreply.github.com>
2018-11-20 14:13:55 +0100

    Create LinearRegression.py

---
+++ LinearRegression.py
@@ -0,0 +1,32 @@
+from sklearn.linear_model import LinearRegression
+import numpy as np
+import pandas as pd
+import csv
+import sys
+
+sc_expression_data = sys.argv[1]
+human_tfs = sys.argv[2]
+
+test_data = pd.read_table(sc_expression_data, index_col=0)
+tfs = pd.read_table(human_tfs, index_col=0, names='TF')
+reg = LinearRegression()
+
+X = test_data.loc[test_data.index.intersection(tfs.index)].T
+coefs = pd.DataFrame(index=X.columns)
+
+for target in test_data.index:
+    # Get the expression values of target
+    y = test_data.loc[target]
+    # Expression of TFs
+    X_local = X.loc[:,X.columns != target]
+    # Run regression
+    reg.fit(X_local, y)
+    # Update the coefficients (regulatory relationships)
+    coefs[target] = 0
+    coefs.loc[X_local.columns,target] = reg.coef_
+
+network = coefs.reset_index().melt(id_vars="index")
+
+network
+
+print(network.to_csv(sep="\t", quoting=csv.QUOTE_NONE))
